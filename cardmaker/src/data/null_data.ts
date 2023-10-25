4e16c823ccae6412550c6f9324c74e2f5525ceb5
krexer008 <y.fedor86@mail.ru>
2023-10-26 02:26:54 +0300

    data_start

---
+++ cardmaker/src/data/null_data.ts
@@ -0,0 +1,42 @@
+import {
+    Position,
+    Size,
+    ColorType,
+    Base64,
+    Link,
+    Block,
+    TextBlock,
+    ImageBlock,
+    GraphicBlock,
+    CanvasType,
+    EditorType,
+    Hystory
+} from "../type/type.js";
+
+let positionValue: Position;
+let sizeValue: Size;
+let colorValue: ColorType;
+let base64Value: Base64;
+let linkValue: Link;
+let blockValue: Block;
+let textValue: TextBlock;
+let imageBlockValue: ImageBlock;
+let graphicBlockValue: GraphicBlock;
+let canvasValue: CanvasType;
+let editorValue: EditorType;
+let hystoryValue: Hystory;
+
+export {
+    positionValue,
+    sizeValue,
+    colorValue,
+    base64Value,
+    linkValue,
+    blockValue,
+    textValue,
+    imageBlockValue,
+    graphicBlockValue,
+    canvasValue,
+    editorValue,
+    hystoryValue
+};
