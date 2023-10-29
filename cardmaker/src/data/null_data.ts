94819886f2b8843d6f7435d25fc757e31dceb51e
krexer008 <y.fedor86@mail.ru>
2023-10-30 00:39:25 +0300

    1

--- cardmaker/src/data/null_data.ts
+++ cardmaker/src/data/null_data.ts
@@ -1,27 +1,27 @@
 import {
-    TextBlock,
-    ImageBlock,
-    ArtBlock,
-    CanvasType,
-    EditorType,
-    Template,
-    Hystory
-} from "../type/type.js";
+  TextBlock,
+  ImageBlock,
+  ArtBlock,
+  CanvasType,
+  EditorType,
+  Template,
+  Hystory,
+} from '../type/type.js'
 
-let textBlockValue: TextBlock;
-let imageBlockValue: ImageBlock;
-let artBlockValue: ArtBlock;
-let canvasValue: CanvasType;
-let editorValue: EditorType;
-let templateValue: Template;
-let hystoryValue: Hystory;
+let textBlockValue: TextBlock
+let imageBlockValue: ImageBlock
+let artBlockValue: ArtBlock
+let canvasValue: CanvasType
+let editorValue: EditorType
+let templateValue: Template
+let hystoryValue: Hystory
 
 export {
-    textBlockValue,
-    imageBlockValue,
-    artBlockValue,
-    canvasValue,
-    editorValue,
-    hystoryValue,
-    templateValue
-};
+  textBlockValue,
+  imageBlockValue,
+  artBlockValue,
+  canvasValue,
+  editorValue,
+  hystoryValue,
+  templateValue,
+}
