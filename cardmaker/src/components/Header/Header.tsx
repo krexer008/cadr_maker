7f90d676b504bdbfb58e275a2e1fc900c21dbd60
krexer008 <y.fedor86@mail.ru>
2023-11-16 07:36:32 +0300

    4-Component Start

---
+++ cardmaker/src/components/Header/Header.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import s from './Header.module.css'
+import New_project from './New_project/New_project'
+import Save_project from './Save_project/Save_project'
+import Save_as_project from './Save_as_project/Save_as_project'
+import Open_project from './Open_project/Open_project'
+import Exit from './Exit/Exit'
+
+function Header() {
+  return (
+    <div className={s.name}>
+      <New_project />
+      <Save_project />
+      <Save_as_project />
+      <Open_project />
+      <Exit />
+    </div>
+  )
+}
+export default Header
