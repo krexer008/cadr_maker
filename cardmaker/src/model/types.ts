cabb1e4d32953ba4504315db4f619118d632ba3f
krexer008 <y.fedor86@mail.ru>
2024-01-15 07:37:51 +0300

    Redux started

---
+++ cardmaker/src/model/types.ts
@@ -0,0 +1,103 @@
+enum ArtValue {
+    Arrow,
+    Circle,
+    Line,
+    Quote,
+    Rectangle,
+}
+
+type Position = {
+    left: number;
+    top: number;
+};
+type Size = {
+    width: number;
+    height: number;
+};
+
+type ImageType = {
+    type: "link" | "base64" | "";
+    data: string;
+};
+
+type ColorType = {
+    r: number;
+    g: number;
+    b: number;
+    a: number;
+};
+
+type TextStyle = {
+    fontSize: number;
+    fontFamily: string;
+    fontWeight: "bold" | "normal";
+    fontStyle: "normal" | "italic";
+    textDecoration: "underline" | "none";
+    color: ColorType;
+};
+
+// Object type
+
+type Block = {
+    id: string;
+    size: Size;
+    position: Position;
+};
+
+type TextBlockType = Block & {
+    type: "text";
+    value: string;
+    textStyle: TextStyle;
+};
+
+type ImageBlockType = Block & {
+    type: "image";
+    image: ImageType;
+};
+
+type ArtBlockType = Block & {
+    type: "art";
+    value: ArtValue;
+    bgColor: ColorType;
+    borderColor: ColorType;
+};
+
+type ActiveBlock = string;
+type ActiveCanvas = string;
+
+type Canvas = {
+    id: string;
+    size: Size;
+    filtr: ColorType;
+    bgImage: ImageType;
+    bgColor: ColorType;
+    blocks: Array<TextBlockType | ImageBlockType | ArtBlockType>;
+    activeBlock: ActiveBlock;
+};
+
+type Template = {
+    id: string;
+    canvas: Canvas;
+};
+
+type Editor = {
+    canvas: Canvas;
+    templates: Array<Template>;
+};
+
+export type {
+    TextBlockType,
+    ImageBlockType,
+    ArtBlockType,
+    Template,
+    Canvas,
+    Editor,
+    ColorType,
+    ImageType,
+    Size,
+    Position,
+    ActiveBlock,
+    ActiveCanvas,
+};
+
+export { ArtValue };
